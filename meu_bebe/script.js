be96f426432657e80be9c46c749605cde5d645e1
mltomacheski <160253044+mltomacheski@users.noreply.github.com>
2025-02-18 08:03:05 -0300

    java script

---
+++ meu_bebe/script.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome completo");;
+var email = prompt("Digite seu email");;
+var cartao = prompt("Digite o número de seu cartão");;
+var senha = prompt("Digite a senha");;
+
+console.log(nome,email,cartao,senha);
